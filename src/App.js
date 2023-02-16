90345ea9f73616226035145783d9d018b4a006c0
aleksmn <al-mn@yandex.ru>
2023-02-16 19:15:13 +0300

    add not found

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Books from './components/books';
 import Home from './components/home';
 import About from './components/about';
 import Contacts from './components/contacts';
+import NotFound from './components/notFound';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/books" element={< Books />} />
         <Route path="/about" element={< About />} />
         <Route path="/contacts" element={< Contacts />} />
+        <Route path="*" element={< NotFound />} />
         {/* <Route path="/" element={<Navigate to="/books" />} /> */}
 
       </Routes>
