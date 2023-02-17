ed91a7fbf82e664ae75b537b826bd1ff16451b38
aleksmn <al-mn@yandex.ru>
2023-02-17 20:54:56 +0300

    add info to book page

--- src/components/book.jsx
+++ src/components/book.jsx
@@ -1,11 +1,23 @@
 import { useParams } from "react-router-dom"
+import { getBooks } from '../services/bookService';
 
 const Book = () => {
+
     const { id } = useParams()
+    const book = getBooks().filter(b => b._id === id)[0]
+
+    // console.log(id)
+    // console.log(book)
+
     return (
-
-        <h1>Книга {id}</h1>
-
+        <>
+            <h1>{book.title}</h1>
+            <ul>
+                <li>Автор: {book.author}</li>
+                <li>Жанр: {book.genre['name']}</li>
+                <li>{book.pages} стр.</li>
+            </ul>
+        </>
     );
 }
 
