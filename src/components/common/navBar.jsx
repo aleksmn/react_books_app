28cd11b7d600697a3309626332fe885c3c26d5ea
aleksmn <al-mn@yandex.ru>
2023-02-17 18:32:37 +0300

    fix styles

--- src/components/common/navBar.jsx
+++ src/components/common/navBar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
     return (
         <nav className="mb-3 navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
-                <Link to="/" className="navbar-brand">Navbar</Link>
+                <Link to="/" className="navbar-brand">MyBooks</Link>
                 <button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
