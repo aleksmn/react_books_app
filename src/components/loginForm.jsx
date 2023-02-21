204e5a6367538435a41a5d021d4e4d5d78aafc1e
aleksmn <al-mn@yandex.ru>
2023-02-21 13:47:56 +0300

    add form design

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -5,8 +5,25 @@ class LoginForm extends Component {
 
     render() {
         return (
-            <div>
+            <div className='container' style={{'max-width':'600px'}}>
                 <h1>Логин</h1>
+
+                <form>
+                    <div className="mb-3">
+                        <label for="exampleInputEmail1" className="form-label">Электронная почта</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        <div id="emailHelp" className="form-text">Мы не делимся вашими контактами</div>
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleInputPassword1" className="form-label">Пароль</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" />
+                    </div>
+                    <div className="mb-3 form-check">
+                        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                        <label className="form-check-label" for="exampleCheck1">Присылать оповещения на почту</label>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Отправить</button>
+                </form>
 
 
             </div>
