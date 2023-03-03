ef5e5cea1c61af474700206c843aa0c775f41ca5
aleksmn <al-mn@yandex.ru>
2023-03-03 12:30:24 +0300

    add handle submit placeholder

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -2,25 +2,31 @@ import React, { Component } from 'react';
 
 
 class LoginForm extends Component {
+    handleSubmit = e => {
+        e.preventDefault();
+
+        console.log('Отправлено!')
+    }
+
 
     render() {
         return (
-            <div className='container' style={{'max-width':'600px'}}>
+            <div className='container' style={{'maxWidth':'600px'}}>
                 <h1>Логин</h1>
 
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div className="mb-3">
-                        <label for="exampleInputEmail1" className="form-label">Электронная почта</label>
+                        <label htmlFor="exampleInputEmail1" className="form-label">Электронная почта</label>
                         <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                         <div id="emailHelp" className="form-text">Мы не делимся вашими контактами</div>
                     </div>
                     <div className="mb-3">
-                        <label for="exampleInputPassword1" className="form-label">Пароль</label>
+                        <label htmlFor="exampleInputPassword1" className="form-label">Пароль</label>
                         <input type="password" className="form-control" id="exampleInputPassword1" />
                     </div>
                     <div className="mb-3 form-check">
                         <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                        <label className="form-check-label" for="exampleCheck1">Присылать оповещения на почту</label>
+                        <label className="form-check-label" htmlFor="exampleCheck1">Присылать оповещения на почту</label>
                     </div>
                     <button type="submit" className="btn btn-primary">Отправить</button>
                 </form>
